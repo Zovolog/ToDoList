344b88c6cdda73bf1c8d09d55fa32eb6c04429f2
@Zovolog <zimoror@gmail.com>
2021-09-18 00:39:44 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('#input');
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+const total = document.querySelector('#total');
+let i = 0;
+btn.addEventListener('click',(e)=>{
+  if(input.value === '') return;
+  createDeleteElements(input.value)
+  input.value = ''
+})
+
+function createDeleteElements(value){
+  i++
+  const li = document.createElement(`li`);
+  const btn = document.createElement(`button`);
+  li.className = `li`;
+  li.textContent = value
+
+  btn.className = `btn`;
+  btn.textContent = `Удалить`;
+  li.appendChild(btn);
+
+  btn.addEventListener('click', e=>{
+    i--
+    total.textContent = i;
+    result.removeChild(li);
+  })
+
+  li.addEventListener('click',(e)=>{
+    li.classList.toggle(`li-active`)
+  })
+
+  total.textContent = i;
+  result.appendChild(li)
+}
